4b2d1624a1fbe0b4a5c52300e23781bcc9d9a51d
ani-ec2 <ani-ec2@testaws.cc>
2018-09-21 21:54:09 +0000

    created a new branch with only c_programs folder and deleted other files

--- addnum1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sum=0
-for i in $*
-do
-sum=`expr $i + $sum`
-done
-echo "Sum of given numbers $* is $sum"
--- printnum1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-var1=1
-while [ $var1 -le $1 ] ;do
-   echo "$var1"
-var1=`expr $var1 + 1`
-done
